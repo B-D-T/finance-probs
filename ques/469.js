13f00ca6e02d95873d831841d78620e05e3680f8
BDT <43049623+B-D-T@users.noreply.github.com>
2025-04-29 11:21:43 -0400

    Update 469.js
    Finished work I started with a previous commit (https://github.com/B-D-T/finance-probs/commit/c988648fd15ffc48be7201fc9779cd66a70d4e30). The issue was I had a sparse Array, which means it had length but no values and thus I couldn't iterate over it using `.map()`.
    
    I also added a little more feedback in the solution.

--- ques/469.js
+++ ques/469.js
@@ -8,16 +8,15 @@ function fnQues469 ($, objFromMain) {
     this.defineVariables = function(){
         
         // Originally there were 3 IRR problems on the same page. However, since I need unique data-ansboxKey names, I limited it to 1 per page
-        let inv01 = Array(udf.uRand(5,9));
+        let inv01 = Array.from({ length: udf.uRand(5, 9) }, () => 0); // Initialize array with 0s
         const aryInvestments = [inv01];
-        aryInvestments.forEach(elem=> elem[0]= -1 * udf.uRand(14000,44000,2000) ); // Initial investment in year 0
-        
+        aryInvestments.forEach(elem => elem[0] = -1 * udf.uRand(14000, 44000, 2000)); // Initial investment in year 0
+
         // Build the cash flow timeline with random values (This is overwritten in next step. So why is it here??)
         function populateInvestmentTL(ary){
             return ary.map((value, idx) => 
                 idx === 0 ? value : udf.uRand(8000, 10000, 200)
             );
-        
         }
 
         const quesVars = {
@@ -30,9 +29,13 @@ function fnQues469 ($, objFromMain) {
             }
         };
         const strNoIRRFound = `This code was unable to calculate an IRR for the investment`;
+        const codeCouldCalcIRR = Number(calcVars.calcTheAns.ansIRR) ? true : false;
         const displayVars = {
-            dispIRRInv01: Number(calcVars.calcTheAns.ansIRR) ? udf.uRound(calcVars.calcTheAns.ansIRR,6) : strNoIRRFound
+            dispCodeCouldCalcIRR: codeCouldCalcIRR,
+            dispIRRInv01: codeCouldCalcIRR ? udf.uRound(calcVars.calcTheAns.ansIRR,6).toFixed(5) : strNoIRRFound,
+            dispIRRInv01AsPerc: codeCouldCalcIRR ? (calcVars.calcTheAns.ansIRR * 100).toFixed(2) : strNoIRRFound
         };
+        console.log('displayVars', displayVars);
 
         return udf.combineVarObjs(quesVars, calcVars, displayVars);
     }
@@ -44,33 +47,60 @@ function fnQues469 ($, objFromMain) {
         obj.ansBoxMessage = udf.ansBoxMessages("writeOutNums"); //decimalPlaces4
 
         obj.stem = `
-        <p>
+        <div style="margin-bottom:2em;">
             The following is a table of cash flows.
             Find the internal rate of return (IRR)
             by inputting the numbers into MS Excel and using the IRR function.
-        </p>
-        <p>
-            Enter your answer as a decimal with at least 4 decimal places.
-            By default, Excel returns IRR as as percent.
-            If you don't know how to format a cell as a decimal, watch
-            <a href="https://exceljet.net/lessons/what-is-a-number-format-in-excel" target="_blank">
-                this Exceljet video</a> for a brief explanation of number formatting.
-            Or, just go into a empty cell and type =1*A1
-            (substituting A1 for your cell that has the IRR calculation);
-            that will often return the percent as a decimal.
-        </p>
-        <div>
+        </div>
+        <div style="margin-bottom: 0.5em;">
             <div id="cfTableInv01Ques469">${capbudg.htmlCashFlowTable(v.varInvTL01)}</div>
             <div id="respIRR01Ques469">${capbudg.htmlRespIRR()}</div>
         </div>
+        <div style="margin-top: -1em; margin-bottom:0.5em;">
+            You need to submit your answer as a <b>decimal</b>
+            with at least 4 decimal places.
+            However, Excel returns IRR as as percent.
+            If you don't know how to format a cell as a decimal, watch
+            <a href="https://exceljet.net/lessons/what-is-a-number-format-in-excel" target="_blank">
+                this Exceljet video</a> for
+            a brief explanation of number formatting.
+        </div>
+        <div style="margin-top:0.5em; font-size: 0.9em;">
+            Or, just go into a empty cell and type =1*A1
+            (substituting A1 for your cell that has the IRR calculation);
+            that will often return the percent as a decimal.
+        </div>
         `;
 
-
         obj.solution = `
-        <p>
-        IRR of investment: <b>${v.dispIRRInv01}</b>
-        </p>
-        `;        
+        <div>
+            The IRR of the investment is <b>${v.dispIRRInv01}</b>.
+        </div>
+        <div style="margin-top: 1em;">
+            <p>
+                As a reminder, 
+                the IRR is the rate at which the net present value (NPV) of an investment is zero.
+                In other words, it is the discount rate at which the present value of
+                future cash flows equals the initial investment.
+            </p>
+        </div>
+        <div id="addl-irr-context-if-available">
+        `;
+        if (v.dispCodeCouldCalcIRR) {
+            obj.solution += `
+            <p>
+                That means that if the cost of capital is less than ${v.dispIRRInv01AsPerc}%,
+                this investment results in a positive NPV.
+            </p>
+            <p>
+                On the flip side, 
+                if the cost of capital is greater than ${v.dispIRRInv01AsPerc}%,
+                this investment results in a negative NPV.
+            </p>
+            `;
+        }
+        obj.solution += `</div>`;
+
         return obj;
 
     } // end of pageContent
